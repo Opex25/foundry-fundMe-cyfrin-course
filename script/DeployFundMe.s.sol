2c8ec101ffa9863fb52afd501f03bb1592dbaaa7
Opeyemi <opexunited25@gmail.com>
2024-08-25 15:17:26 +0100

    modules

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License_Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+
+contract DeployFundMe is Script {
+
+    HelperConfig helperConfig = new HelperConfig();
+    address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+    function run() public returns (FundMe) {
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
+}
